8a545863c007775d5f2dd42557fb194ef17bc3b0
r613 <icreatemyweb@gmail.com>
2019-01-31 00:08:01 +0200

    Add files via upload
    v6.4 - stable (bugs not found yet, and this time I looked!)

--- Create.py
+++ Create.py
@@ -1,17 +1,12 @@
-#v6.3 - beta 
-#fixing problem
-#don't print long message before entering every line
-
 from Input import Input
 
 def create():
   size_h = 9
   size_w = 9
   list = []
-  print "you can enter the puzzle in the code itself (there are instructions inside to see how), or you may just enter it here"
-    print "Please enter 9 numbers you may enter a space between each group of numbers,"
   for row_no in range(size_h): #we run one row at a time
-    print " ---------"
+    print "you can enter the puzzle in the code itself (there are instructions inside to see how), or you may just enter it here"
+    print "Please enter 9 numbers you may enter a space between each group of numbers,  "
     list.append(row_creator())
   
   return fix_matrix(list)
@@ -31,7 +26,7 @@ def fix_matrix(matrix):
 
 def row_creator():
   try:
-    row_list = raw_input(" ").split(" ") # row_list would be a list the user enters (like 1,2,3) and then split by ',' into a list
+    row_list = raw_input(": ").split(" ") # row_list would be a list the user enters (like 1,2,3) and then split by ',' into a list
     new_list = []
     for nums in row_list:
       for num in nums:
